85e3d26af48a4a4ca4bcfed5e488344f1f09c4ec
jaeykim <jaeykim@umich.edu>
2017-10-24 11:53:13 -0400

    defined a wavefunction class for future generalization on potentials

--- particle_in_box.py
+++ particle_in_box.py
@@ -14,90 +14,104 @@ import numpy as np
 # Should be in strings with x as the variable
 # Function f should be infinitely differentiable
 # and have values close to 0 at x = 0 and x = L
-f1 = "( np.sin(3 * np.pi * x / L))"
+f1 = "( np.sin(3 * np.pi * x / self.L))"
 f2 = "( np.e ** (-(x - 3) ** 2) )"
-f3 = "( x * np.sin(5 * np.pi * x / L ) )"
-f4 = "( np.sin( np.pi * x ** 3 / L ** 3 ) )"
+f3 = "( x * np.sin(5 * np.pi * x / self.L ) )"
+f4 = "( np.sin( np.pi * x ** 3 / self.L ** 3 ) )"
 
 # Actual function that is to be evaluated
-psi = f2
 
-# Constant Declaration
-L=10.0                          # length of the box
-numBoxes = 1000                 # Number of points to plot
-dx = L / numBoxes               # X distance between each plot
-maxN = 70                       # Maximum quantum number to check
+# Global constants
 h = 1                           # 6.626*10**-34 planck's constant
 hBar = h / (2 * np.pi)
-m = 1                           # mass. for an electron, 9.109*10**-31
-
-
-# Input function f (in strings with x as a variable) integrate from a to b
-# Uses Reimann sum limits
-def integrate(f):
-    total=0.0
-    for i in range(numBoxes):
-        x = dx*i
-        total = total + eval(f)*dx
-
-    return total
 
 # Returns n^th element of orthonormal basis of solutions of the Schrodinger's Equation
-def wave(n):
-    return "(np.sqrt(2.0/L) * np.sin(" + str(n) + "*x*np.pi/L))"
+def particle_in_box(n, L):
+    return "(np.sqrt(2.0/" + str(L) + ") * np.sin(" + str(n) + "*x*np.pi/" + str(L) + "))"
+# Can simulate all other potentials as long as there is a orthonormal basis function defined as
+# basis(n,L) that returns a string of n^th orthonormal basis
 
-# Returns the normalization constant of psi
-def normalConst(psi):
-    return str(1.0 / np.sqrt(integrate( psi + "*" + psi)) )
+
+class Wavefunction:
+    def __init__(self, psi_in, basis_in, L_in, numPoints_in, maxN_in, mass_in):
+        self.psi = psi_in
+        self.L = float(L_in)
+        self.numPoints = numPoints_in
+        self.basis = basis_in
+        self.maxN = maxN_in
+        self.mass = mass_in
+
+        self.xs = [self.L / self.numPoints * i for i in range(self.numPoints + 1)]
+        self.normalize()
+        self.components = self.getComponents()
+        self.energyCoef = self.getEnergy()
+
+    # Input function f (in strings with x as a variable) integrate from a to b
+    # Uses Reimann sum limits
+    def integrate(self, f):
+        total=0.0
+        dx = self.L / self.numPoints
+        for i in range(self.numPoints):
+            x = dx*i
+            total = total + eval(f)*dx
+
+        return total
+
+    # Normalizes Psi
+    def normalize(self):
+        self.psi = str(1.0 / np.sqrt(self.integrate( self.psi + "*" + self.psi)) ) + "*" + self.psi
+
+    # Returns the array of coefficients in the fourier series transform of psi
+    def fourierTransform(self):
+        coef = []
+        for n in range(1,self.maxN+1):
+                f = self.psi + "*"+ self.basis(n, self.L)
+                coef.append(self.integrate(f))
+        return coef
+
+    # returns the array of energy of each quantum level
+    def getEnergy(self):
+            energy = []
+            for n in range(1, self.maxN + 1):
+                    energy.append(n ** 2 * h ** 2 / (8 * self.mass * self.L ** 2))
+            return energy
+
+    def getComponents(self):
+        coef = self.fourierTransform()
+        components = [coef[n - 1] * np.array([eval(self.basis(n, self.L)) for x in self.xs]) for n in range(1,self.maxN+1)]
+        return components
+
         
-# Normalize psi
-psi = normalConst(psi) + "*"+ psi
 
-# Returns the array of coefficients in the fourier series transform of psi
-def fourierTransform(psi):
-    coef = []
-    for n in range(1,maxN+1):
-            f = psi + "*"+ wave(n)
-            coef.append(integrate(f))
-    return coef
+    # Returns the probability density (psi^*psi) at time t
+    def prob(self,t):
+            prob = []
+            realComp = np.zeros(self.numPoints + 1)
+            imgComp = np.zeros(self.numPoints + 1)
+            for n in range(self.maxN):
+                    realComp = realComp + np.cos(hBar * self.energyCoef[n] * t) * np.array(self.components[n])
+                    imgComp = imgComp + np.sin(hBar * self.energyCoef[n] * t) * np.array(self.components[n])
+            prob = np.sqrt(np.square(realComp) + np.square(imgComp))
+            return prob
 
-# returns the array of energy of each quantum level
-def getEnergy():
-        energy = []
-        for n in range(1, maxN + 1):
-                energy.append(n**2 * h ** 2 / (8*m*L**2))
-        return energy
+    def simulate(self, t_start, t_end, scaleFactor):
+        plt.ion()
 
-coef = fourierTransform(psi)
-energy = getEnergy()
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        line, = ax.plot(psi.xs, self.prob(t_start), 'r-')
+        for t in range(t_start, t_end):
+            scaleFactor = 10
+            line.set_ydata(psi.prob(t * scaleFactor))
+            fig.canvas.draw()
+        plt.close()        
 
-xs = [dx*i for i in range(numBoxes + 1)]
+    
 
-components = []
-for n in range(1, maxN + 1):
-        components.append( coef[n - 1] * np.sin( np.array(xs) * n * np.pi / L )  )
 
-# Returns the probability density (psi^*psi) at time t
-def prob(t):
-        prob = []
-        realComp = np.zeros(numBoxes + 1)
-        imgComp = np.zeros(numBoxes + 1)
-        for n in range(maxN):
-                realComp = realComp + np.cos(hBar * energy[n] * t) * components[n]
-                imgComp = imgComp + np.sin(hBar * energy[n] * t) * components[n]
-        prob = np.sqrt(np.square(realComp) + np.square(imgComp) )
-        return prob
 
-psiSqrd = prob(0)
 
-# Display
-plt.ion()
+psi = Wavefunction(f2, particle_in_box, 10, 500, 40, 1)
+psi.simulate(0,500,10)
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-line, = ax.plot(xs, prob(0), 'r-')
-for t in range(5000):
-    scaleFactor = 10
-    line.set_ydata(prob(t * scaleFactor))
-    fig.canvas.draw()
-plt.close()
+
